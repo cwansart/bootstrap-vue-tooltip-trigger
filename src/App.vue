dc11797c0229b5c4555176ac12d23261030c5b74
Christian Wansart <christian.wansart@openknowledge.de>
2020-01-10 20:38:52 +0100

    Prepare example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <NavItem tooltip="The start of this web page" tooltip-trigger="focus">Start</NavItem>
+      <NavItem tooltip="My personal projects" tooltip-trigger="hover">Projects</NavItem>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavItem from './components/NavItem'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    NavItem
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li>
+    <a href="#" :title="tooltip" v-b-tooltip="{ trigger: tooltipTrigger }">
+      <slot/>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    tooltip: {
+      type: String,
+      default: ''
+    },
+    tooltipTrigger: {
+      type: String,
+      default: 'hover'
+    }
+  }
+}
+</script>
